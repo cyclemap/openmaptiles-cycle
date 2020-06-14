dadbe23d98bfb1f613fdbb484f450e44772f15d4
adrian <adrian-sw@aporter.org>
2020-06-14 08:34:33 -0400

    update is working again.

--- update.sh
+++ update.sh
@@ -6,37 +6,33 @@
 
 minimumSize=9500000000
 locationName=north-america
-pbfFile=$locationName-latest.osm.pbf
-#newFile=$locationName-new.osm.pbf
+pbfFile=$locationName.osm.pbf
+newFile=$locationName-new.osm.pbf
 
 set -e #exit on failure
 
 exec &> >(tee --append "update.log")
 
-rm -f $pbfFile
-wget https://download.geofabrik.de/$pbfFile
-mv $pbfFile data/$pbfFile
+#rm --force $pbfFile
+#wget --progress=bar:force:noscroll https://download.geofabrik.de/$pbfFile
+#mv --force $pbfFile data/$pbfFile
 
 #THE NEXT STEP:
 #see here:  https://wiki.openstreetmap.org/wiki/Osmupdate#Assembling_an_OSM_Change_file
 #you can create a "change file" (osc file) and use Osmosis or osm2pgsql to get that change file into postgres.
 #we have to stop using quickstart.sh for that to work.
 
-#THIS IS NOW BROKEN.  not sure what happened, but osmupdate now breaks other downstream products.
-#specifically this:  make import-borders
-#gives this error:  terminate called after throwing an instance of 'osmium::not_found'
-#what():  location for one or more nodes not found in node location index
-#echo updating:  started at $(date)
-#
-#rm --force data/$newFile
-#docker-compose run --rm import-osm osmupdate --verbose /import/$pbfFile /import/$newFile
-#mv --force data/$newFile data/$pbfFile
+echo updating:  started at $(date)
+rm --force data/$newFile
+docker-compose run --rm openmaptiles-tools osmupdate --verbose /import/$pbfFile /import/$newFile
+mv --force data/$pbfFile{,.old}
+mv --force data/$newFile data/$pbfFile
 
-if [ $(stat --format=%s data/$pbfFile) -lt $minimumSize ]; then
-	#sometimes the file is too small because something failed.  let's stop here because this needs fixing.
-	echo $pbfFile file size too small.  expected minimum size of $minimumSize bytes.
-	false
-fi
+#if [ $(stat --format=%s data/$pbfFile) -lt $minimumSize ]; then
+#	#sometimes the file is too small because something failed.  let's stop here because this needs fixing.
+#	echo $pbfFile file size too small.  expected minimum size of $minimumSize bytes.
+#	false
+#fi
 
 echo updating:  done at $(date)
 echo "====================================================================="
