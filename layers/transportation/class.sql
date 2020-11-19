95d8eee6e7517677c873a6df5dd8243185a4d06a
adrian <adrian-sw@aporter.org>
2020-11-19 14:37:24 -0500

    using mtb:scale as a hint for cycle trails and surface.

--- layers/transportation/class.sql
+++ layers/transportation/class.sql
@@ -19,6 +19,8 @@ SELECT CASE
         WHEN tags->'rcn' IN ('yes') THEN 'cycleway'
         WHEN tags->'lcn' IN ('yes') THEN 'cycleway'
         WHEN tags->'bicycle' IN ('designated', 'mtb') THEN 'cycleway'
+        WHEN tags->'mtb:scale' NOT IN ('6') THEN 'cycleway'
+        WHEN tags->'mtb:scale:imba' IS NOT NULL THEN 'cycleway'
         WHEN tags->'bicycle' IN ('yes', 'permissive', 'dismount') AND (
             highway IN ('pedestrian', 'living_street', 'path', 'footway', 'steps', 'bridleway', 'corridor', 'track', 'residential', 'service', 'unclassified') OR
             (tags->'maxspeed' ~ E'^\\d+ mph$' AND replace(tags->'maxspeed', ' mph', '')::integer <= 35) OR
@@ -92,6 +94,8 @@ SELECT CASE
                THEN 'unpaved'
            WHEN tags->'footway' IN ('crossing') THEN 'paved'
            WHEN tags->'bicycle' IN ('mtb') THEN 'unpaved'
+           WHEN tags->'mtb:scale' IS NOT NULL THEN 'unpaved'
+           WHEN tags->'mtb:scale:imba' IS NOT NULL THEN 'unpaved'
            WHEN highway IN ('motorway', 'trunk', 'primary', 'secondary', 'tertiary', 'unclassified', 'residential', 'living_street', 'road', 'service', 'motorway_link', 'trunk_link', 'primary_link', 'secondary_link', 'tertiary_link', 'raceway', 'steps', 'cycleway') THEN 'paved'
            WHEN tags->'hiking' IN ('yes', 'designated', 'permissive') THEN 'unpaved'
            ELSE NULL
