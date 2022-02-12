11cd6924be1200996336b97c66c5dd0513cc91a5
adrian <adrian-sw@aporter.org>
2022-02-11 20:48:06 -0500

    minor:  move a log.

--- timing.sh
+++ timing.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-egrep --text --no-filename '^quickstart: done at|^      : Git version|^Imposm took|Time: .*\([^()]*:[^()]*:[^()]*\)|^real' logs/update*.log update.log
+egrep --text --no-filename '^quickstart: done at|^      : Git version|^Imposm took|Time: .*\([^()]*:[^()]*:[^()]*\)|^real' logs/update*.log
 
