72813b05e6fee1ff30336baaf4be90098e6ada78
adrian <adrian-sw@aporter.org>
2022-07-27 18:55:34 -0400

    different timing logs.

--- timing.sh
+++ timing.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-egrep --text --no-filename '^quickstart: done at|^      : Git version|^Imposm took|Time: .*\([^()]*:[^()]*:[^()]*\)|^real' logs/update*.log |
-	sed --regexp-extended -e 's/Time:.*\((.*)\..*\)/\1/' -e 's/.*Git version.*: (.*)/\1/' |
-	sed --regexp-extended ':a;N;$!ba;s/\n/ /g;s/quickstart: done at .{32}/&\n/g;'
+#egrep --text --no-filename '^quickstart: done at|^      : Git version|^Imposm took|Time: .*\([^()]*:[^()]*:[^()]*\)|^real' logs/update*.log |
+#	sed --regexp-extended -e 's/Time:.*\((.*)\..*\)/\1/' -e 's/.*Git version.*: (.*)/\1/' |
+#	sed --regexp-extended ':a;N;$!ba;s/\n/ /g;s/quickstart: done at .{32}/&\n/g;'
+
+
+egrep --text --no-filename '^Time:.* d |^==========|^quickstart|NOTICE:  (aggregate highest_highway|Finished layer poi)' logs/update*.log |
+	uniq
 
