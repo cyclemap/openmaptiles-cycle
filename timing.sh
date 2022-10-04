a5070f5a45d5d85332414f508b089519d6ca7468
adrian <adrian-sw@aporter.org>
2022-10-04 10:41:46 -0400

    new timing script.  found out that copy concurrency needs to go back to 30.

--- timing.sh
+++ timing.sh
@@ -4,7 +4,36 @@
 #	sed --regexp-extended -e 's/Time:.*\((.*)\..*\)/\1/' -e 's/.*Git version.*: (.*)/\1/' |
 #	sed --regexp-extended ':a;N;$!ba;s/\n/ /g;s/quickstart: done at .{32}/&\n/g;'
 
+#egrep --text --no-filename '^Time:.*( d |\(.*:.*:.*\))|Generating zoom .*\.\.\.$|Tile generation complete!$|^(updating|quickstart): (done|started) at' logs/update*.log
 
-egrep --text --no-filename '^Time:.* d |^==========|^quickstart|NOTICE:  (aggregate highest_highway|Finished layer poi)' logs/update*.log |
-	uniq
+
+last=$(date +%s)
+<logs/update.log \
+	egrep --text --no-filename 'Generating zoom .*\.\.\.$|Tile generation complete!$|^(updating|quickstart): (done|started) at' |
+	tail -n100 |
+	while read line; do
+		<<<$line egrep --text -q 'Generating zoom .*\.\.\.$|Tile generation complete!$' && {
+			time=$(<<<$line cut -b1-19 |sed --regexp-extended 's/ (.{2})-(.{2})-(.{2})/ \1:\2:\3/')
+			comment=$(<<<$line cut -b21-)
+			#different timezone.  fun
+			time=$(date +%s --utc --date="$time")
+			difference=$(($time-$last))
+			last=$time
+			<<<$line egrep -q 'Tile generation complete!$' && {
+				comment="$lastComment $comment"
+			} || {
+				lastComment=$comment
+				comment='finished importing'
+			}
+			
+			echo "$(($difference/3600)) $comment"
+		}
+		<<<$line egrep -q '^(updating|quickstart): (done|started) at' && {
+			time=$(<<<$line sed 's/.* at //')
+			time=$(date +%s --date="$time")
+			last=$time
+			
+			<<<$line egrep -q '^updating: started at' && echo
+		}
+done
 
